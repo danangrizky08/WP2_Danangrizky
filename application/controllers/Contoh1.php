97bc0069a5c1a983ad7691eff867b184a4788ff6
danangrizky08 <rizkydanang999@gmail.com>
2024-03-27 20:28:25 +0700

    tugas contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Danang Rizky
+ Saya tinggal di daerah Pondok ungu
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
+?>
